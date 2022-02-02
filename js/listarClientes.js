abb6ee55fd89bd808c8af18d35c6374ace46fb67
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-02-02 12:43:53 -0300

    momeny js add

--- js/listarClientes.js
+++ js/listarClientes.js
@@ -3,25 +3,28 @@ const listarClientes = document.querySelector('#listar-clientes')
 const motarLista = () => {
   let dataUsers = JSON.parse(localStorage.getItem('dataUsers') || '[]')
 
+  let dataNow = moment()
+  console.log(dataNow)
+
   dataUsers.forEach(client => {
     const cliente = document.createElement('tr')
-    let dataNow = new Date()
-    console.log(dataNow)
-    let dataClient = client.data
-    console.log(dataClient)
-    function valueData() {
-      if(dataClient >= dataNow.getTime()) {
-      return ` <td class="bg-danger text-white text-center">${client.data}</td>`
-      }else if(dataClient <= dataNow.getTime()) {
-      return ` <td class="bg-success text-white text-center">${client.data}</td>`
-      }
-    }
+    
+    // let dataClient = client.data
+    // console.log(dataClient)
+    // function valueData() {
+    //   if(dataClient >= dataNow.getTime()) {
+    //   return ` <td class="bg-danger text-white text-center">${client.data}</td>`
+    //   }else if(dataClient <= dataNow.getTime()) {
+    //   return ` <td class="bg-success text-white text-center">${client.data}</td>`
+    //   }
+    //   console.log(dataClient)
+    // }
     cliente.innerHTML = `
       <td>${client.Nome}</td>
       <td>${client.endereco}</td>
       <td>${client.telefone}</td>
       <td>${client.valor}</td>
-      ${valueData()}
+      <td>${client.data}</td>
     `
     listarClientes.appendChild(cliente)
   })
