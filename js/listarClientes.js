d72a16a65bc27411536d7bafa30db2560dad2b80
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-02-02 17:10:38 -0300

    add momentjs from format date

--- js/listarClientes.js
+++ js/listarClientes.js
@@ -1,31 +1,43 @@
 const listarClientes = document.querySelector('#listar-clientes')
+document.addEventListener('click', (e) => {
+ if (e.target.classList.contains('btn-exc')) {
+  let dataUsers = JSON.parse(localStorage.getItem('dataUsers') || '[]')
+  let index = e.target.parentElement.parentElement.rowIndex - 1
+  dataUsers.splice(index, 1)
+  localStorage.setItem('dataUsers', JSON.stringify(dataUsers))
+  e.target.parentElement.parentElement.remove()
+ }
+})
 
 const motarLista = () => {
   let dataUsers = JSON.parse(localStorage.getItem('dataUsers') || '[]')
 
-  let dataNow = moment()
-  console.log(dataNow)
-
   dataUsers.forEach(client => {
     const cliente = document.createElement('tr')
-    
-    // let dataClient = client.data
-    // console.log(dataClient)
-    // function valueData() {
-    //   if(dataClient >= dataNow.getTime()) {
-    //   return ` <td class="bg-danger text-white text-center">${client.data}</td>`
-    //   }else if(dataClient <= dataNow.getTime()) {
-    //   return ` <td class="bg-success text-white text-center">${client.data}</td>`
-    //   }
-    //   console.log(dataClient)
-    // }
+
+    let dataClient = client.data
+    let dataClientFormat = moment(dataClient).format('DD/MM/YYYY')
+  
+
+    function valueData() {
+      if (dataClientFormat < moment().format('DD/MM/YYYY')) {
+        return ` <td class="bg-danger text-white text-center">${dataClientFormat}</td>`
+      } else if (dataClientFormat > moment().format('DD/MM/YYYY')) {
+        return ` <td class="bg-success text-white text-center">${dataClientFormat}</td>`
+      }
+    }
     cliente.innerHTML = `
       <td>${client.Nome}</td>
       <td>${client.endereco}</td>
       <td>${client.telefone}</td>
       <td>${client.valor}</td>
-      <td>${client.data}</td>
+      ${valueData()}
+      <td><button class="btn btn-danger btn-exc"">excluir</button></td> 
     `
+    function clearUser() {
+      localStorage.removeItem('listUsers')
+      window.location.href = './main.html'
+    }
     listarClientes.appendChild(cliente)
   })
 }
